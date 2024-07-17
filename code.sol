3cbdd08fe140a2fad03b364724ea1a6861bffcbb
Karanveer Mittal <karan.mittal2579@gmail.com>
2024-07-17 22:26:20 +0530

    code.sol

--- code.sol
+++ code.sol
@@ -1,18 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
-contract SmartContractProject {
-    function requireInstance (uint a, uint b) public pure {
-        require ( a + b < 10, "Make sure the sum of the numbers entered is less than 10.");
+contract FunctionErrors {
+    function requireC(uint _UPSCAge) 
+    public pure returns (string memory){
+        uint _UPSCAge = 21;
+        require(_upscAge >= _UPSCAge, "You doesn't pass eligibility criteria,You are under 21");
+        return ("You are eligible to give the exam");
     }
-
-    function assertInstance (uint c) public pure {
-        assert ( c >= 5 );
+    
+    function assertC(uint _userAge) 
+    public pure returns (string memory){
+        uint _LegalAge = 60;
+        assert(_userAge >= _LegalAge);
+        return("You are eligible to get pension");
     }
-
-    function revertInstance (uint d, uint e) public pure {
-        if ( d + e > 10) {
-            revert ("Make sure the sum of the numbers entered is less than 10.");
+    
+    function revertC(uint _amount) 
+    public pure {
+        uint _Balance = 3000;
+        if (_amount > _Balance) {
+            revert("You didn't have sufficient balance");
         }
     }
 }
